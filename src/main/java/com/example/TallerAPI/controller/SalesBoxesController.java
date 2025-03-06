2714fd371f262ac7ebc7284f705833800a457a09
JuanCorrea4 <jecorreajara@gmail.com>
2025-03-05 22:08:33 -0500

    Add docker folder and resolve problem in salexBoxesController

--- src/main/java/com/example/TallerAPI/controller/SalesBoxesController.java
+++ src/main/java/com/example/TallerAPI/controller/SalesBoxesController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/api/products")
+@RequestMapping("/api/salesBoxes")
 @RestController
 public class SalesBoxesController {
 
